9e0ff68bf84ff762922576fceadc83e7775e3325
Sandacus <sandyarl329@gmail.com>
2025-12-05 00:40:26 +0000

    day04 part 2

--- src/solutions/day04.rs
+++ src/solutions/day04.rs
@@ -139,9 +139,93 @@ fn check_down_left(i: usize, j: usize, arr: &[Vec<char>]) -> u64 {
 }
 
 
-pub fn part_two(_input: &str) -> u64 {
+pub fn part_two(input: &str) -> u64 {
     println!("Solving part 2!");
-    42
+    
+    let mut arr: Vec<Vec<char>> = input
+        .lines()
+        .collect::<Vec<&str>>()
+        .iter()
+        .map(|x| x.chars().collect())
+        .collect::<Vec<Vec<char>>>();
+
+    // println!("{:?}", arr); 
+
+    let mut accessible: u64 = 0;
+    let mut found_paper = true;
+    while found_paper {
+        let (paper_removed, flag) = find_accessible(accessible, &mut arr);
+        accessible += paper_removed;
+        found_paper = flag;
+        println!("{:?}", (paper_removed, flag));
+        println!("while loop, loop!");
+    }
+    
+    accessible
+}
+
+fn find_accessible(count: u64, arr: &mut[Vec<char>]) -> (u64, bool) {
+    let mut paper_removed = false;
+    let mut accessible: u64 = 0;
+    // loop through array
+    for i in 0..arr.len() {
+        for j in 0..arr[i].len() {
+            if arr[i][j] != '@' {
+                continue;
+            }
+
+            let mut count: u64 = 0;
+
+            // check left
+            if j > 0 {
+                count += check_left(i, j, arr);
+            }
+
+            // check right
+            if j < arr[i].len()-1 {
+                count += check_right(i, j, arr);
+            }
+
+            // check top
+            if i > 0 {
+                count += check_up(i, j, arr);
+            }
+
+            // check down
+            if i < arr.len()-1 {
+                count += check_down(i, j, arr);
+            }
+
+            // check top left
+            if j > 0 && i > 0 {
+                count += check_up_left(i, j, arr);
+            }
+
+            // check top right
+            if j < arr[i].len()-1 && i > 0 {
+                count += check_up_right(i, j, arr);
+            }
+
+            // check bottom right
+            if j < arr[i].len()-1 && i < arr.len()-1 {
+                count += check_down_right(i, j, arr);
+            }
+
+            // check bottom left
+            if j > 0 && i < arr.len()-1 {
+                count += check_down_left(i, j, arr);
+            }
+            
+            if count < 4 {
+                println!("[{}, {}]", i, j);
+                accessible += 1;
+                arr[i][j] = '.';
+                paper_removed = true;
+            }
+        }
+    }
+
+    (accessible, paper_removed)
 }
 
 
@@ -169,11 +253,10 @@ mod tests {
 
 
     #[test]
-    #[ignore]
     fn part_two_example_input() {
         let input = fs::read_to_string("data/examples/04.txt")
         .expect("Should have been able to read the file");
-        assert_eq!(part_two(&input), 0);
+        assert_eq!(part_two(&input), 43);
     }
 
 
