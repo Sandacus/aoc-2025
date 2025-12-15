536f358911022fc79ffd60d8687217852b56cb71
Sandacus <sandyarl329@gmail.com>
2025-12-15 23:29:51 +0000

    first attempt at day08 part 1

--- src/solutions/day08.rs
+++ src/solutions/day08.rs
@@ -1,5 +1,7 @@
 // day 8 solution
 
+const NUMBER_OF_PAIRS: usize = 1_000;
+
 // point struct
 #[derive(Debug)]
 #[derive(PartialEq)]
@@ -42,16 +44,22 @@ fn calcualte_distance(p1: &Point, p2: &Point) -> f32 {
 }
 
 #[allow(dead_code)]
-fn add_connections(mut conns: Vec<Connection>, con: &Connection) -> Vec<Connection> {
-    if conns.len() < 10 {
+fn add_connections(mut conns: Vec<Connection>, con: Connection) -> Vec<Connection> {
+    if conns.len() < NUMBER_OF_PAIRS {
         conns.push(con.clone());
     } else {
-        for (i, old_con) in conns.clone().into_iter().enumerate() {
-            if con.dist > old_con.dist {
-                conns.remove(i);
-                conns.insert(i, con.clone());
-                break;
-            } 
+        // find longest distance
+        let mut idx_longest = 0;
+        for (i, c) in conns.iter().enumerate().skip(1) {
+            if c.dist > conns[idx_longest].dist {
+                idx_longest = i;
+            }
+        }
+
+        // update conns vector
+        if con.dist < conns[idx_longest].dist {
+            conns.remove(idx_longest);
+            conns.push(con);
         }
     }
 
@@ -81,23 +89,71 @@ pub fn part_one(input: &str) -> u64 {
         .collect::<Vec<Point>>();
 
     // for the points add connections
-    // only keep the top 10 shortest connections
     let mut conns: Vec<Connection> = Vec::new();
     for (i, p1) in points.iter().enumerate() {
         for (j, p2) in points.iter().enumerate().skip(i+1) {
             let con: Connection = Connection::new((p1.clone(), i), (p2.clone(), j));
-            conns = add_connections(conns, &con);
+            conns = add_connections(conns, con);
         }
     }
 
-    println!("Print connections");
-    for con in conns {
-        println!("conns: {:?}", con.id);
+    // println!("Print connections");
+    // for con in &conns {
+        // println!("conns: {:?}", con.id);
+    // }
+
+    // sort connections into circuits
+    // initialise with first two connected points
+    // let mut circuits: Vec<Vec<usize>> = vec![vec![conns[0].id.0, conns[0].id.1]];
+    let mut circuits: Vec<Vec<usize>> = Vec::new();
+    // start looping through connections
+    for i in 0..conns.len() {
+        let p1: &usize = &conns[i].id.0;
+        let p2: &usize = &conns[i].id.1;
+        let mut circuit_found = false;
+        for j in 0..circuits.len() {
+            // check if p1 or p2 in circuit
+            if circuits[j].contains(p1) && !circuits[j].contains(p2) {
+                circuits[j].push(*p2);
+                circuit_found = true;
+                break;
+            } else if circuits[j].contains(p2) && !circuits[j].contains(p1) {
+                circuits[j].push(*p1);
+                circuit_found = true;
+                break;
+            } else if circuits[j].contains(p2) && circuits[j].contains(p1) {
+                circuit_found = true;
+                break;
+            }
+        }
+        if !circuit_found {
+            circuits.push(vec![*p1, *p2]);
+        }
     }
 
+    println!("Print circuits\n");
+    let mut first: u64 = 0;
+    let mut second: u64 = 0;
+    let mut third: u64 = 0;
+    for c in circuits {
+        println!("circuit: {:?}", c);
+        if c.len() as u64 >= first {
+            third = second;
+            second = first;
+            first = c.len() as u64;
+        } else if c.len() as u64 >= second {
+            third = second;
+            second = c.len() as u64;
+        } else if c.len() as u64 > third {
+            third = c.len() as u64;
+        }
+    }
+    
 
-    42
+    first * second * third
 }
+
+// 4590 - too low
 
 #[allow(dead_code)]
 pub fn part_two(_input: &str) -> u64 {
@@ -161,7 +217,7 @@ mod tests {
         let p2_id: usize = 2;
         let con = Connection::new((p1, p1_id), (p2, p2_id));
         let mut conns: Vec<Connection> = Vec::new();
-        conns = add_connections(conns, &con);
+        conns = add_connections(conns, con);
         
         assert_eq!(conns.len(), 1);
     }
