379302523fb3c3e9d36eb197c5d9064d14ea8e3e
Sandacus <sandyarl329@gmail.com>
2025-12-21 22:47:12 +0000

    scaffold for day09

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use std::fs;
 use crate::solutions::*;
+
 mod solutions;
 
 fn main() {
-    let input = fs::read_to_string("data/inputs/08.txt")
+    let input = fs::read_to_string("data/inputs/09.txt")
         .expect("Should have been able to read the file");
     
-    let part1 = day08::part_one(&input);
+    let part1 = day09::part_one(&input);
     println!("part 1 ans: {}", part1);
 
-    let part2 = day08::part_two(&input);
+    let part2 = day09::part_two(&input);
     println!("part 2 ans: {}", part2);
 }
---
+++ src/solutions/day09.rs
@@ -0,0 +1,54 @@
+// day 9 solution
+
+#[allow(dead_code)]
+pub fn part_one(_input: &str) -> u64 {
+    println!("Solving part 1!");
+    
+
+    42
+}
+
+
+#[allow(dead_code)]
+pub fn part_two(_input: &str) -> u64 {
+    println!("Solving part 2!");
+
+    
+    42
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
+
+    const EXAMPLE_FILE: &str = "data/examples/09.txt";
+    const INPUT_FILE: &str = "data/inputs/09.txt";
+
+    #[test]
+    fn read_input() {
+        assert!(fs::exists(INPUT_FILE).expect("couldn't find input"));
+    }
+
+    #[test]
+    fn read_example() {
+        assert!(fs::exists(EXAMPLE_FILE).expect("couldn't find example"));
+    }
+
+    #[test]
+    fn part_one_example_input() {
+        let input = fs::read_to_string(EXAMPLE_FILE)
+        .expect("Should have been able to read the file");
+        assert_eq!(part_one(&input), 50);
+    }
+
+    #[test]
+    #[ignore]
+    fn part_two_example_input() {
+        let input = fs::read_to_string(EXAMPLE_FILE)
+        .expect("Should have been able to read the file");
+        assert_eq!(part_two(&input), 40);
+    }
+
+}
