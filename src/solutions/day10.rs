0c333de790f27d1bcb9b63edda93d5353fe403b4
Sandacus <sandyarl329@gmail.com>
2025-12-23 22:12:16 +0000

    fun with iterators on day10 input parsing

--- src/solutions/day10.rs
+++ src/solutions/day10.rs
@@ -1,10 +1,45 @@
 // day 10 solution
 
 #[allow(dead_code)]
-pub fn part_one(_input: &str) -> u64 {
+pub fn part_one(input: &str) -> u64 {
     println!("Solving part 1!");
 
+    // parse input
+    let lines: Vec<&str> = input.lines().collect();
+
+    // get diagrams
+
+    // get wiring
+
+    // get joltage
+
+
    42
+}
+
+fn get_diagrams(lines: Vec<&str>) -> Vec<Vec<char>> {
+    lines
+        .iter()
+        .map(|x| 
+            x.split_whitespace().next().unwrap().chars().filter(|x| *x != '[' && *x != ']').collect()
+        )
+        .collect::<Vec<Vec<char>>>()
+}
+
+fn get_wiring(lines: Vec<&str>) -> Vec<Vec<Vec<u64>>> {
+    lines
+        .iter()
+        .map(|x| 
+            x.split_whitespace()
+                .filter(|y| y.contains("("))
+                .map(|x| 
+                    x.chars()
+                        .filter_map(|c| c.to_digit(10).map(u64::from))
+                        .collect::<Vec<u64>>()
+                )
+                .collect::<Vec<Vec<u64>>>()
+        )
+        .collect::<Vec<Vec<Vec<u64>>>>()
 }
 
 
@@ -39,6 +74,40 @@ mod tests {
         let input = fs::read_to_string(EXAMPLE_FILE)
         .expect("Should have been able to read the file");
         assert_eq!(part_one(&input), 7);
+    }
+
+    #[test]
+    fn test_parse_diagrams() {
+        let lines = vec!["[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}", 
+            "[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}", 
+            "[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}"];
+        
+        let diagrams: Vec<Vec<char>> = get_diagrams(lines);
+
+        let expected_diagrams0 = vec!['.', '#', '#', '.'];
+        let expected_diagrams1 = vec!['.', '.', '.', '#', '.'];
+        let expected_diagrams2 = vec!['.', '#', '#', '#', '.', '#'];
+
+        assert_eq!(diagrams[0], expected_diagrams0);
+        assert_eq!(diagrams[1], expected_diagrams1);
+        assert_eq!(diagrams[2], expected_diagrams2);
+    }
+
+    #[test]
+    fn test_parse_wiring() {
+        let lines = vec!["[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}", 
+            "[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}", 
+            "[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}"];
+        
+        let wiring = get_wiring(lines);
+
+        let expected_wiring0 = vec![vec![3], vec![1,3], vec![2], vec![2,3], vec![0,2], vec![0,1]];
+        let expected_wiring1 = vec![vec![0,2,3,4], vec![2,3], vec![0,4], vec![0,1,2], vec![1,2,3,4]];
+        let expected_wiring2 = vec![vec![0,1,2,3,4], vec![0,3,4], vec![0,1,2,4,5], vec![1,2]];
+
+        assert_eq!(wiring[0], expected_wiring0);
+        assert_eq!(wiring[1], expected_wiring1);
+        assert_eq!(wiring[2], expected_wiring2);
     }
 
     #[test]
