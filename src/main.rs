78f40ef57754580c1b72bf54819ec7ac9b82f880
Sandacus <sandyarl329@gmail.com>
2025-12-24 21:41:32 +0000

    added input parsing to day11, trying out using lifetimes with borrowed values

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ use crate::solutions::*;
 mod solutions;
 
 fn main() {
-    let input = fs::read_to_string("data/inputs/10.txt")
+    let input = fs::read_to_string("data/inputs/11.txt")
         .expect("Should have been able to read the file");
     
-    let part1 = day10::part_one(&input);
+    let part1 = day11::part_one(&input);
     println!("part 1 ans: {}", part1);
 
-    let part2 = day10::part_two(&input);
+    let part2 = day11::part_two(&input);
     println!("part 2 ans: {}", part2);
 }
--- src/solutions/day11.rs
+++ src/solutions/day11.rs
@@ -1,10 +1,30 @@
 // day 11 solution
 
+use std::collections::HashMap;
+
 #[allow(dead_code)]
-pub fn part_one(_input: &str) -> u64 {
+pub fn part_one(input: &str) -> u64 {
     println!("Solving part 1!");
 
+    let lines: Vec<&str> = input.lines().collect();
+
+    let map = get_device_outputs(&lines);
+
+    
+
    42
+}
+
+fn get_device_outputs<'a>(lines: &[&'a str]) -> HashMap<&'a str, Vec<&'a str>> {
+    let mut map: HashMap<&str, Vec<&str>> = HashMap::new();
+
+    for line in lines {
+        let k = line.split(":").next().unwrap();
+        let v = line.split(" ").skip(1).collect();
+        map.insert(k, v);
+    }
+
+    map
 }
 
 
@@ -39,6 +59,24 @@ mod tests {
         let input = fs::read_to_string(EXAMPLE_FILE)
         .expect("Should have been able to read the file");
         assert_eq!(part_one(&input), 5);
+    }
+
+    #[test]
+    fn test_parse_input() {
+        let input: String = "aaa: you hhh
+you: bbb ccc
+bbb: ddd eee
+ccc: ddd eee fff".to_string();
+        let lines: Vec<&str> = input.lines().collect();
+        let map = get_device_outputs(&lines);
+
+        let mut expected_map = HashMap::new();
+        expected_map.insert("aaa", vec!["you", "hhh"]);
+        expected_map.insert("you", vec!["bbb", "ccc"]);
+        expected_map.insert("bbb", vec!["ddd", "eee"]);
+        expected_map.insert("ccc", vec!["ddd", "eee", "fff"]);
+
+        assert_eq!(map, expected_map);
     }
 
     #[test]
